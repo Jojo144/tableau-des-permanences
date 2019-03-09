601bf6c80eac1fb24b12608d44cca80230e64015
Jojo144 <jojo144@girole.fr>
2019-03-09 19:14:18 +0100

    Better urls

--- permanences/urls.py
+++ permanences/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    # url(r'^contact/$', views.contact, name='contact'),
-    url(r'^details/(?P<pk>[0-9]+)/$', views.details, name='details'),
-    url(r'^texte$', views.text, name='text'),
-    url(r'^$', views.index, name='index'),
+    re_path(r'^details/(?P<pk>[0-9]+)$', views.details, name='details'),
+    path('texte', views.text, name='text'),
+    path('', views.index, name='index'),
 ]
